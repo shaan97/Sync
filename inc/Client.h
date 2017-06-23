9ac2b435b6e09e8b7d39ecd877dfa8f7bdcdff36
= <shaankaranmathur@gmail.com>
2017-06-23 03:35:49 -0700

    Added private members to Client class. These are part of the boost::asio library, and are necessary for establishing a socket connection with the server.

--- inc/Client.h
+++ inc/Client.h
@@ -1,6 +1,9 @@
 #ifndef SHAAN97_SYNC_CLIENT_H
 #define SHAAN97_SYNC_CLIENT_H
 
+#include <memory>
+#include <boost/asio.hpp>
+
 namespace shaan97 {
 
 namespace sync {
@@ -9,14 +12,35 @@ typedef unsigned long GROUP_ID;
 
 class Client {
 	private:
-		GROUP_ID gid; // Group ID
+		// Group ID
+		GROUP_ID gid;
+
+		// Keeps track of whether we have a valid connection
 		bool connectedToGroup = false;
 
-		bool connect(GROUP_ID gid);
+		// Main object for conducting I/O in boost::asio
+		std::shared_ptr<boost::asio::io_service> io_service;
+
+		// Used to resolve server name into an endpoint
+		std::shared_ptr<boost::asio::ip::tcp::resolver> resolver;
+
+		// Used to store the name of the server and name of the service
+		std::shared_ptr<boost::asio::ip::tcp::resolver::query> query;
+
+		// Used to iterate over list of endpoints
+		std::shared_ptr<boost::asio::ip::tcp::resolver::iterator> endpoint_iterator;
+		
+		// Socket by which we communicate with the server
+		std::shared_ptr<boost::asio::ip::tcp::socket> socket;
+
+		// Can be used to read what failed if error occurred for I/O
+		boost::system::error_code error;
+		
+		
 	public:
 		Client(GROUP_ID gid = 0);
 		Client(const Client& c);
-		~Client();
+		virtual ~Client();
 
 		void setGroup(GROUP_ID gid) {
 			this->gid = gid;
@@ -35,6 +59,9 @@ class Client {
 
 		// Plays music in group queue. Blocks until queue is empty
 		void play();
+
+		// Plays music in group queue. Asynchronous, returns after requesting to play
+		void async_play();
 		
 		void swap(Client& c);
 		Client& operator=(const Client& c);
