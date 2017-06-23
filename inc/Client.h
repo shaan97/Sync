682aa46ef644d3aacdd777122256c9b9c9c2fc89
= <shaankaranmathur@gmail.com>
2017-06-23 02:49:26 -0700

    Initial commit of Client class. This is going to be used for the client program to interface with the server.

---
+++ inc/Client.h
@@ -0,0 +1,46 @@
+#ifndef SHAAN97_SYNC_CLIENT_H
+#define SHAAN97_SYNC_CLIENT_H
+
+namespace shaan97 {
+
+namespace sync {
+
+typedef unsigned long GROUP_ID;
+
+class Client {
+	private:
+		GROUP_ID gid; // Group ID
+
+	public:
+		Client(GROUP_ID gid = 0);
+		Client(const Client& c);
+		~Client();
+
+		void setGroup(GROUP_ID gid) {
+			this->gid = gid;
+		}
+
+		GROUP_ID getGroup() const {
+			return this->gid;
+		}
+
+		bool syncGroup();
+		bool syncGroup(GROUP_ID gid);
+
+		
+		void swap(Client& c);
+		Client& operator=(const Client& c);
+
+
+};
+
+
+
+
+}
+
+}
+
+
+
+#endif
