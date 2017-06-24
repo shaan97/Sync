a4fc9a8ea5a5634f18b059683cbb62ce951b3643
= <shaankaranmathur@gmail.com>
2017-06-23 18:03:11 -0700

    Simple implementations of Member::write, Member::read, and other Member methods

---
+++ src/Member.cpp
@@ -0,0 +1,42 @@
+#include "../inc/Member.h"
+
+using namespace shaan97::sync;
+
+Member::Member(const std::string& name, const std::shared_ptr<boost::asio::ip::tcp::socket>& socket, const std::shared_ptr<Group>& group) :
+	name(name), client_socket(socket)
+{
+	groups.emplace(group);
+}
+
+Member::~Member() {
+	// Intentionally blank
+}
+
+template <class Buffer>
+std::size_t Member::write(Buffer b, std::size_t len) {
+	return boost::asio::write(*(this->client_socket), boost::asio::buffer(b, len), this->error);
+}
+
+template <class Buffer>
+std::size_t Member::read(Buffer b, size_t len) const {
+	return boost::asio::read(boost::asio::buffer(b, len), this->error);
+}
+
+template <class Buffer>
+std::size_t Member::read(Buffer b) const {
+	return boost::asio::read(boost::asio::buffer(b), this->error);
+}
+
+void Member::swap(Member &m) {
+	std::swap(this->name, m.name);
+	std::swap(this->client_socket, m.client_socket);
+	std::swap(this->error, m.error);
+	std::swap(this->groups, m.groups);
+}
+
+Member& Member::operator=(const Member &m) {
+	// Copy and Swap Idiom
+	Member copy(m);
+	swap(copy);
+	return *this;
+}
