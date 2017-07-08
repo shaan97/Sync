41acb5dce3cff5a565ebc8d69759c615ffa52695
= <shaankaranmathur@gmail.com>
2017-07-08 11:15:28 -0700

    Initial design of Member.h with comments

--- inc/Member.h
+++ inc/Member.h
@@ -6,45 +6,68 @@
 #include <boost/asio.hpp>
 #include <unordered_set>
 
+#include "Error.h"
 
 
-class Group;
+
+
 
 namespace shaan97 {
 
 namespace sync {
+	class Group;
 
+	typedef std::string MemberName;
 class Member {
-	private:
-		std::string name;
-		std::shared_ptr<boost::asio::ip::tcp::socket> client_socket;
-		boost::system::error_code error;
-		std::unordered_set<std::shared_ptr<Group>> groups;
 	public:
-		Member(const std::string& name, const std::shared_ptr<boost::asio::ip::tcp::socket>& socket, const std::shared_ptr<Group>& group);
-		Member(const Member& m);
+
+		/// Constructor requiring the Member's name, a socket over which to communicate, and the group to which he/she belongs
+		Member(const MemberName& name, const std::shared_ptr<boost::asio::ip::tcp::socket>& socket, const std::shared_ptr<Group>& group);
+		Member(const Member& m) = delete;	// No copy constructor : we don't want two references to the same socket
+		Member(Member&& m);
 		virtual ~Member();
 
-		std::string getName() const {
-			return this->name;
-		}
+		// Accessor
+		MemberName getName() const;
 
-		void setName(const std::string& name) {
-			this->name = name;
-		}
+		// Mutators
+		void setName(const MemberName& name);
+		void setName(const MemberName& name, Error& e);
 
+		/// Write the contents of the Buffer into the socket
+		/// @param Buffer		Reference to the first element in the buffer
+		/// @param len			Size of the buffer
+		/// @param e			Set to an boost::system::error_code value in case it occurs
+		/// @return std::size_t	Number of elements in buffer written over the socket
 		template <class Buffer>
-		std::size_t write(Buffer b, std::size_t len);
+		std::size_t write(Buffer b, std::size_t len, Error& e);
 
+		/// Read the contents of the socket into the Buffer
+		/// @param Buffer		Reference to the first space in the buffer
+		/// @param len			Size of the buffer
+		/// @param e			Set to an boost::system::error_code value in case it occurs
+		/// @return std::size_t	Number of elements in buffer after reading the socket
 		template <class Buffer>
-		std::size_t read(Buffer b, size_t len) const;
+		std::size_t read(Buffer b, std::size_t len, Error& e) const;
 
+		/// Read the contents of the socket into the Buffer
+		/// @param Buffer		Reference to the first space in the buffer
+		/// @param e			Set to an boost::system::error_code value in case it occurs
+		/// @return std::size_t	Number of elements in buffer after reading the socket
+		/// @note In this overload, the Buffer must behave like a container which has a size() method
 		template <class Buffer>
-		std::size_t read(Buffer b) const;
-
-		void swap(Member& m);
+		std::size_t read(Buffer b, Error& e) const;
 		
-		Member& operator=(const Member& m);
+		Member& operator=(const Member& m) = delete; // No assignment operator. We don't want two references to the same socket
+		
+		/// Tests to see if two Members are the same
+		bool operator==(const Member& m) const;
+
+	protected:
+		MemberName name;
+		std::shared_ptr<boost::asio::ip::tcp::socket> client_socket;
+		std::unordered_set<std::shared_ptr<Group>> groups;	// One day we might want to support many groups
+		
 };
 
 }
