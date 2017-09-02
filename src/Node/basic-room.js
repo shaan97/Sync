beafd286a03127f30290c96ec56a7f53b8687c06
= <shaankaranmathur@gmail.com>
2017-09-01 17:39:41 -0700

    Map member names to members to allow easy removal.

--- src/Node/basic-room.js
+++ src/Node/basic-room.js
@@ -1,6 +1,8 @@
 var util = require("util");
 var Status = require("./globals").Status;
 var EventEmitter = require("events").EventEmitter;
+var RequestType = require("./globals").RequestType;
+var requestTypeToString = require("./globals").requestTypeToString;
 
 /* TODO : Implement rooms */
 class BasicRoom extends EventEmitter {
@@ -11,63 +13,85 @@ class BasicRoom extends EventEmitter {
 		this.room_name = room_name;
 
 		// Initialize set of members 
-		this.members = new Set();
+		this.members = new Object();
 		this.insert(admin);
 		this.makeAdmin(admin);
+
+		// Music "queue" (might allow arbitrary promotions)
+		this.music_queue = new Array();
+
 	}
 
 	insert(member) {
 		util.log("Attempting to add " + member.name);
-		if(member in this.members)
+		if(member.name in this.members)
 			return false;
 
-		this.members.add(member);
+		this.members[member.name] = member;
 		var room = this;
 		
 		// Make the room handle all messages from the connection now
-		member.connection.on("message", function(message) {
+		member.on("message", function(message) {
 			util.log("Received message from " + member.name);
 			room.handleMessage(member, message);
 		});
 
-		member.connection.on("close", function() {
+		member.on("close", function() {
 			util.log("Receiving close from " + member.name);
-			room.remove(member);
+			room.remove(member.name);
 		});
 
-		util.log("Sending success.");
-		member.connection.send(Status.SUCCESS);
-
+		member.send(Status.SUCCESS);
+		util.log(member.name + " added to " + this.room_name);
 		return true;
 	}
 
-	remove(member) {
-		util.log("Attempting to remove " + member.name);
-		if(!this.members.has(member))
+	remove(member_name) {
+		util.log("Attempting to remove " + member_name);
+		if(!(member_name in this.members))
 			return false;
 
+		var member = this.members[member_name];
 		if(member !== this.admin) {
-			this.members.delete(member);
+			this.members.removeAttribute(member_name);
 			member.close();
 		}
 		else {
-			util.log(member.name + " is an admin. Attempting to get new admin.");
+			util.log(member_name + " is an admin. Attempting to get new admin.");
 			this.getNewAdmin();
 			if(member === this.admin) {
 				this.emit("empty");
 			} else {
-				this.members.delete(member);
+				this.members.removeAttribute(member_name);
 				member.close();
 			}
 		}
 
-		util.log(member.name + " was removed.");
+		util.log(member_name + " was removed.");
 		return true;
 	}
 
 	handleMessage(member, message) {
-		/* TODO : Use Member's decoder to get message details. */
-		util.log("Received message from member: %s", message)
+		var log = "Received message from member: ";
+		util.log(log + requestTypeToString());
+		member.decoder.message = message;
+		// Determine action based on request type
+		switch(member.decoder.RequestType()) {
+		case RequestType.SONG_REQUEST:
+			return false; // TODO 
+		case RequestType.REMOVE_MEMBER:
+			var other = member.decoder.getOtherMemberName();		
+			if(member != this.admin || other == null || !(other in this.members)) {
+				ws.send(Status.FAIL);
+				return false;
+			}
+			this.remove(other);
+			return true;
+		default:
+			member.send(Status.INVALID);
+			return false;
+		}
+		
 	}
 
 	size() {
@@ -76,7 +100,7 @@ class BasicRoom extends EventEmitter {
 
 	makeAdmin(member) {
 		util.log("Making " + member.name + " admin.");
-		if(!this.members.has(member) && !insert(member))
+		if(!(member.name in this.members) && !this.insert(member))
 			return false;
 
 		this.admin = member;
