777d2a9fe0192eb7b4ef6cfe04979a23c4bcd751
= <shaankaranmathur@gmail.com>
2017-09-01 17:42:16 -0700

    New Requests for adding songs and removing members

--- src/Node/globals.js
+++ src/Node/globals.js
@@ -1,12 +1,35 @@
 var RequestType = {
 	ROOM_CREATE : 'C',
-	ROOM_JOIN : 'J'
+	ROOM_JOIN : 'J',
+	SONG_REQUEST : 'S',
+	INVALID_REQUEST : '\0',
+	REMOVE_MEMBER : 'R'
 };
+
+function requestTypeToString(req) {
+	switch(req) {
+	case ROOM_CREATE:
+		return "ROOM_CREATE";
+	case ROOM_JOIN:
+		return "ROOM_JOIN";
+	case SONG_REQUEST:
+		return "SONG_REQUEST";
+	case INVALID_REQUEST:
+		return "INVALID_REQUEST";
+	case REMOVE_MEMBER:
+		return "REMOVE_MEMBER";
+	default:
+		return "UNKNOWN";
+	}
+}
 
 var Status = {
 	SUCCESS : 0,
 	FAIL : 1,
-	PENDING : 2
+	PENDING : 2,
+	EXISTS : 3,
+	NOT_EXIST : 4,
+	INVALID : 5
 }
 
 function deep_copy(obj) {
