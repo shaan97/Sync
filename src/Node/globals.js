ce24628a8b74334b22d1d73a7dc52683605774b1
= <shaankaranmathur@gmail.com>
2017-09-09 02:09:55 -0700

    Added new statuses (i.e. 3 Phase Commit) and added Message Types for sending messages from server to client.

--- src/Node/globals.js
+++ src/Node/globals.js
@@ -3,7 +3,10 @@ var RequestType = {
 	ROOM_JOIN : 'J',
 	SONG_REQUEST : 'S',
 	INVALID_REQUEST : '\0',
-	REMOVE_MEMBER : 'R'
+	REMOVE_MEMBER : 'R',
+	PLAY: '\1',
+	PAUSE: '\2',
+	SKIP: '\3'
 };
 
 function requestTypeToString(req) {
@@ -18,6 +21,12 @@ function requestTypeToString(req) {
 		return "INVALID_REQUEST";
 	case REMOVE_MEMBER:
 		return "REMOVE_MEMBER";
+	case PLAY:
+		return "PLAY";
+	case PAUSE:
+		return "PAUSE";
+	case SKIP:
+		return "SKIP";
 	default:
 		return "UNKNOWN";
 	}
@@ -29,13 +38,24 @@ var Status = {
 	PENDING : 2,
 	EXISTS : 3,
 	NOT_EXIST : 4,
-	INVALID : 5
+	INVALID : 5,
+	CAN_COMMIT : 6,
+	PRE_COMMIT : 7,
+	COMMIT : 8,
+	ABORT_COMMIT : 9,
+	ENQUEUE : 10
 }
 
-function deep_copy(obj) {
-	return JSON.parse(JSON.stringify(obj));
+var MessageType = {
+	PLAY : 0,
+	ENQUEUE_SONG : 1,
+	PAUSE : 2,
+	SKIP : 3,
+	REMOVE_SONG : 4,
+	ADD_MEMBER : 5,
+	REMOVE_MEMBER : 6
 }
 
 exports.RequestType = RequestType;
 exports.Status = Status;
-exports.deep_copy = deep_copy;
+exports.MessageType = MessageType;
