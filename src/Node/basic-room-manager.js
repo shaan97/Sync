12923d2a48efe9373f6297a253f74e04f0dd1a9e
= <shaankaranmathur@gmail.com>
2017-09-10 14:47:26 -0700

    Added comments for maintainability

--- src/Node/basic-room-manager.js
+++ src/Node/basic-room-manager.js
@@ -1,25 +1,44 @@
 var util = require("util");
 
+/*!
+	@class BasicRoomManager
+	
+	@brief Implements the RoomManager API. The room manager is essentially a container
+	for all the rooms.
+
+	@note This may be extended for more intelligent management, which is why we don't simply
+	use some sort of unordered map instead
+*/
 class BasicRoomManager {
 	constructor() {
 		this.rooms = {};
 	}
 
+	/*!
+		@param room			The room to be inserted (@see BasicRoom)
+	*/
 	insert(room) {
+		// If room already exists, fail
 		if(room.room_name in this.rooms)
 			return false;
 		
-		var room_mgr = this;
-		room.on("empty", function() {
-			room_mgr.remove(room);
+		// On the empty event, we have to clean up by removing the room
+		room.on("empty", () => {
+			this.remove(room);
 		});
 
+		// Add the room
 		this.rooms[room.room_name] = room;
 		return true;
 	}
 
+	/*!
+		@param room			The room to be removed (@see BasicRoom)
+	*/
 	remove(room) {
 		util.log("Removing " + room.room_name);
+
+		// Only remove the room if it is in our container
 		if(room.room_name in this.rooms) {
 			room.close();
 			delete this.rooms[room.room_name];
@@ -29,10 +48,14 @@ class BasicRoomManager {
 		return false;
 	}
 
+	/// @param room_name 		The name of the room whose existance we are checking
 	contains(room_name) {
 		return room_name in this.rooms;
 	}
 
+	/// @param room_name		The naem of the room we would like to retrieve
+	///
+	/// @return null if the room doesn't exist
 	getRoom(room_name) {
 		if(room_name in this.rooms) {
 			return this.rooms[room_name];
