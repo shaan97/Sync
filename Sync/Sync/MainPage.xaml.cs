0da9a2449cf3087d5d8abd483fd7a0ba8ee72fd4
Shaan Mathur <shaankaranmathur@gmail.com>
2017-12-25 01:50:03 -0800

    Implemented handlers to send create_room and join_room requests. TODO : Read response

--- Sync/Sync/MainPage.xaml.cs
+++ Sync/Sync/MainPage.xaml.cs
@@ -10,18 +10,36 @@ namespace Sync
 	public partial class MainPage : ContentPage
 	{
 		private SyncServer sync;
-		public MainPage(SyncServer sync)
+        private string member_name;
+		public MainPage(SyncServer sync, string member_name)
 		{
 			this.sync = sync;
+            this.member_name = member_name;
 			InitializeComponent();
 		}
 
-		public void Create_Room(string room) {
-			
+        public void Create_Room(object sender, EventArgs e) {
+            sync.Send(
+                new RequestBuilder
+                {
+                    RequestType = "ROOM_CREATE",
+                    member_name = this.member_name,
+                    room_name = ((Entry)sender).Text
+                }.ToString()
+            );
+            //Navigation.PushAsync()
 		}
 
-		public void Join_Room(string room) {
+		public void Join_Room(object sender, EventArgs e) {
+            sync.Send(
+                new RequestBuilder
+                {
+                    RequestType = "ROOM_JOIN",
+                    member_name = this.member_name,
+                    room_name = ((Entry)sender).Text
+                }.ToString()
+            );
+        }
 
-		}
-	}
+    }
 }
