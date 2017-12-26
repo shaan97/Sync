b8f375a8145f74ac8ae303cdaaccd096a12c6e7e
Shaan Mathur <shaankaranmathur@gmail.com>
2017-12-25 21:52:39 -0800

    Created generic Music Player interface that application will use, blind to actual music player type. SpotifyPlayer is a specific subclass of the player

---
+++ Sync/Sync/IMusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sync
+{
+    public interface IMusicPlayer
+    {
+        bool Play();
+        bool Play(string song, uint start = 0);
+
+        bool Stop();
+
+        bool SkipSong();
+
+        bool Seek(uint seconds);
+        bool SeekTo(uint start);
+
+        bool Login(string username, string password);
+        bool Logout();
+        bool IsLoggedIn();
+
+    }
+}
---
+++ Sync/Sync/SpotifyPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sync
+{
+    public class SpotifyPlayer : IMusicPlayer
+    {
+        public bool IsLoggedIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Play(string song, uint start = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Seek(uint seconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SeekTo(uint start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SkipSong()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
