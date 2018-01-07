616590dd412ac9d580177498f4723dcc52a05a78
Shaan Mathur <shaankaranmathur@gmail.com>
2018-01-07 00:57:20 -0800

    Added debugging statements, comments, and fix bug where request_id field is not in message, which previously caused exceptions to be thrown. Shouldn't happen according to the Sync Protocol, but added for safety against various input

--- Sync/Sync/Sync Server/SyncServer.cs
+++ Sync/Sync/Sync Server/SyncServer.cs
@@ -10,37 +10,54 @@ namespace Sync
 {
     public class SyncServer
     {
+        // Collection of request_id for "address space" of requests. 
+        private const int NUM_IDS = 16;
         protected ConcurrentBag<int> ids;
         private SemaphoreSlim id_available;
 
+        // Queued requests waiting to be sent
         protected ConcurrentQueue<Tuple<Request, Callback>> requests;
         private SemaphoreSlim pending_requests;
+
+        // Registered callbacks for a given active request
         protected Callback[] callbacks;
 
+        // Web socket connection to server
         private WebSocket ws;
         private bool socket_opened;
 
+        // Worker thread that sends pending requests when ID's are available
         private Thread id_manager;
         private volatile bool exit;
 
+        // If SyncEvent received, all subscribers are notified
         public event EventHandler<ResponseDecoder> SyncEvent;
 
         public SyncServer()
         {
-            this.ws = new WebSocket("ws://192.168.1.226:8000/");
+            // Configure server IP address
+            this.ws = new WebSocket("ws://192.168.1.74:8000/");
             this.socket_opened = false;
 
-            this.ids = new ConcurrentBag<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            this.id_available = new SemaphoreSlim(16, 16);
-
-            this.requests = new ConcurrentQueue<Tuple<Request, Callback>>();
-            this.pending_requests = new SemaphoreSlim(0);
-            this.callbacks = new Callback[16];
-
+            // Web socket events
             this.ws.MessageReceived += this.HandleServerResponse;
             this.ws.Closed += (object s, EventArgs e) => { this.socket_opened = false; };
             this.ws.Error += (object s, ErrorEventArgs e) => { this.socket_opened = false; };
 
+            // Set up available ID's
+            this.ids = new ConcurrentBag<int>();
+            for (int i = 0; i < NUM_IDS; i++)
+                this.ids.Add(i);
+            this.id_available = new SemaphoreSlim(NUM_IDS, NUM_IDS);
+
+            // Create queue for requests
+            this.requests = new ConcurrentQueue<Tuple<Request, Callback>>();
+            this.pending_requests = new SemaphoreSlim(0);
+
+            // Create lookup table for callbacks
+            this.callbacks = new Callback[NUM_IDS];
+
+            // Launch worker thread for sending pending requests
             this.exit = false;
             this.id_manager = new Thread(WorkerThreadDeScheduler);
             this.id_manager.Start();
@@ -61,27 +78,45 @@ namespace Sync
         {
             // Enqueue for worker thread to deal with
             this.requests.Enqueue(new Tuple<Request, Callback>(data, callback));
+
+            // Notify existence of pending request
             this.pending_requests.Release();
         }
 
         protected virtual void HandleServerResponse(object sender, MessageReceivedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine($"Received Message:\n{e.Message}");
+
+            // Decode message
             ResponseDecoder response = new ResponseDecoder(e.Message);
-            int request_id = (int)response["request_id"];
-            var sync_event_id = response["sync_event_id"];
-            if(request_id < 0 && sync_event_id == null) {
+            var sync_event_id = response["sync_message"];
+
+            // Only deal with responses to requests; Drop or Forward elsewhere otherwise
+            if(response["request_id"] != null && (int)response["request_id"] < 0 && sync_event_id == null) {
+                // Drop useless packet (invalid request_id and no sync_event_id)
                 return; 
-            } else if(sync_event_id != null) { // sync_event_id and request_id are mutually exclusive due to established protocol
+            } else if(sync_event_id != null) {
+                // This is a sync event. Forward to subscribers and exit
                 SyncEvent.Invoke(sender, response);
+                return;
+            } else if(response["request_id"] == null) {
                 return;
             }
 
+            int request_id = (int)response["request_id"];
+            System.Diagnostics.Debug.WriteLine($"Received request_id {request_id}");
+
+            // Get callback and remove from table
             Callback callback = this.callbacks[request_id];
             this.callbacks[request_id] = null;
+
+            // Make request ID now available to other requests
             this.ids.Add(request_id);
             this.id_available.Release();
+
+            // Invoke appropriate callback based on status
             if (callback == null)
                 return;
-            else if ((int)response["status"] == ResponseDecoder.Status["SUCCESS"]) {
+            else if ((int)response["status"] == ResponseDecoder.Status["SUCCESS"] || (int)response["status"] == ResponseDecoder.Status["PENDING"]) {
                 callback.OnSuccess(sender, response);    
             } else {
                 callback.OnFailure(sender, response);
@@ -130,6 +165,8 @@ namespace Sync
 
                 // Register callback
                 this.callbacks[id] = result.Item2;
+
+                System.Diagnostics.Debug.WriteLine($"Sending following message:\n{result.Item1.ToString()}");
 
                 // Send Request
                 this.ws.Send(result.Item1.ToString());
